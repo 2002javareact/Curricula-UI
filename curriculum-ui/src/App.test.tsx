7fb1d53020d064cede92e3a330478f613f380292
Gerard Cancino <gerard.cancino@gmail.com>
2020-04-01 19:30:56 -0400

    Jest

--- curriculum-ui/src/App.test.tsx
+++ curriculum-ui/src/App.test.tsx
@@ -4,8 +4,9 @@ import App from './App';
 
 // https://github.com/callstack/react-native-testing-library/issues/103
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  expect(getByText(/learn react/i)).not.toBeInTheDocument();
+  //const { getByText } = render(<App />);
+  const { queryByText } = render(<App />);
+  expect(queryByText(/learn react/i)).not.toBeInTheDocument();
   //Original code
   //const linkElement = getByText(/learn react/i);
   //expect(linkElement).not.toBeInTheDocument();
