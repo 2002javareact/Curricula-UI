4f600bfe1e79b5bbdbf837e5144aa6ff1ad318b7
Jesus Cabrero <cabrerojesus@gmail.com>
2020-04-01 12:33:03 -0400

    project-build

---
+++ curriculum-ui/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { store } from './Store';
+import { Provider } from 'react-redux';
+function App() {
+  return (
+    <Provider store = {store}>    
+    <div className="App">
+      <Router> 
+        <Switch> 
+        </Switch>
+          </Router>          
+        </div>
+    </Provider>    
+  );
+}
+
+export default App;
