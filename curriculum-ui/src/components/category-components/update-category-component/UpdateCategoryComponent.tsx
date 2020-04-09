683510e3750faaae233a173fcd90eea603609e1c
Devon James <devjames98@gmail.com>
2020-04-09 14:57:36 -0400

    update category

--- curriculum-ui/src/components/category-components/update-category-component/UpdateCategoryComponent.tsx
+++ curriculum-ui/src/components/category-components/update-category-component/UpdateCategoryComponent.tsx
@@ -1,14 +1,96 @@
 import React from "react";
+import { Form, FormGroup, Label, Col, Input, Button } from "reactstrap";
+import { Category } from "../../../models/Category";
 
 //prop interface
+interface IUpdateCategoryProps {
+  updatedCategory: Category;
+  errorMessage: string;
+  updateCategoryActionMapper: (
+    categoryId: number,
+    categoryColor: string,
+    categoryName: string
+  ) => void;
+}
 
 //state interface?
+interface IUpdateCategoryState {
+  categoryId: number;
+  categoryColor: string;
+  categoryName: string;
+  didSubmit: boolean;
+}
 
-export class UpdateCategoryComponent extends React.Component<any, any> {
+export class UpdateCategoryComponent extends React.Component<
+  IUpdateCategoryProps,
+  IUpdateCategoryState
+> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      //category id should be whatever is passed in from that card (button)
+      categoryId: 0,
+      categoryColor: "",
+      categoryName: "",
+      didSubmit: false
+    };
+  }
+
+  submitUpdate() {}
+
   render() {
     return (
       <>
-        <p>Hello World!</p>
+        <Form onSubmit={this.submitUpdate}>
+          {/* only thing required should be the user id */}
+          <FormGroup row>
+            <Label for="categoryId" sm={2}>
+              CategoryId:
+            </Label>
+            <Col sm={6}>
+              <Input
+                //onChange={this.updateUserId}
+                value={this.state.categoryId}
+                type="number"
+                name="categoryId"
+                id="categoryId"
+                placeholder="CategoryId"
+                required
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="color" sm={2}>
+              Color:
+            </Label>
+            <Col sm={6}>
+              <Input
+                onChange={this.updateColor}
+                value={this.state.categoryColor}
+                type="text"
+                name="color"
+                id="color"
+                placeholder="color"
+              />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="name" sm={2}>
+              Category Name:
+            </Label>
+            <Col sm={6}>
+              <Input
+                onChange={this.updateName}
+                value={this.state.categoryName}
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Category Name"
+              />
+            </Col>
+          </FormGroup>
+          <Button color="success">Update Category</Button>
+        </Form>
       </>
     );
   }
--- curriculum-ui/src/components/category-components/view-all-categories-component/ViewAllCategoriesContainer.ts
+++ curriculum-ui/src/components/category-components/view-all-categories-component/ViewAllCategoriesContainer.ts
@@ -1,18 +1,21 @@
-import { IState } from "../../../reducers"
-import { ViewAllCategoriesComponent } from "./ViewAllCategoriesComponent"
-import { getAllCategoriesActionMapper } from "../../../action-mappers/categories-getall-action-mappers"
-import { connect } from "react-redux"
+import { IState } from "../../../reducers";
+import { ViewAllCategoriesComponent } from "./ViewAllCategoriesComponent";
+import { getAllCategoriesActionMapper } from "../../../action-mappers/categories-getall-action-mappers";
+import { connect } from "react-redux";
 
-const mapStateToProps = (state:IState) => {
-    return {
-        allCategories: state.allCategory.allCategory,
-        //loggedUser: state.loggedUser.loggedUser,
-        errorMessage: state.allCategory.errorMessage        
-    }
-}
+const mapStateToProps = (state: IState) => {
+  return {
+    allCategory: state.allCategory.allCategory,
+    //loggedUser: state.loggedUser.loggedUser,
+    errorMessage: state.allCategory.errorMessage
+  };
+};
 
 const mapDispatchToProps = {
-    getAllCategoriesActionMapper
-}
+  getAllCategoriesActionMapper
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(ViewAllCategoriesComponent)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ViewAllCategoriesComponent);
