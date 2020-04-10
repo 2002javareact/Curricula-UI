c57140db43f7fb65dd730044b4e12f6aa5772a06
usb777 <dzsamoila@gmail.com>
2020-04-10 12:07:20 -0400

    Rename all category files

--- curriculum-ui/src/reducers/category-reducer.ts
+++ curriculum-ui/src/reducers/category-reducer.ts
@@ -1,6 +1,6 @@
 import { ICategoriesState } from ".";
 import { AnyAction } from "redux";
-import { categoriesTypes } from "../action-mappers/categories-getall-action-mappers";
+import { categoriesTypes } from "../action-mappers/getall-categories-action-mappers";
 
 const initialState: ICategoriesState = {
     allCategory:[],
