b8795d105cbe0ced61832828f64c9613c9872c5b
Gerard Cancino <gerard.cancino@gmail.com>
2020-04-14 02:09:11 -0400

    cleaning code

--- curriculum-ui/src/reducers/index.ts
+++ curriculum-ui/src/reducers/index.ts
@@ -1,16 +1,12 @@
 import { combineReducers } from "redux";
 import { Curriculum } from "../models/Curriculum";
-//import { curriculumReducer } from "./get-all-curriculums-reducers";
-//import { updateReducer } from "./update-curriculum-reducer";
 import { Skill } from "../models/Skill";
 import { skillsReducer } from "./skills-reducers";
 import { categoriesReducer } from "./category-reducer";
-//import { getCurriculumByIdReducer } from "./get-curriculum-by-id-reducer";
 import { Category } from "../models/Category";
 import { Visualization } from "../models/Visualization";
 import { visualizationReducer } from "./visualization-reducer";
 import { curriculumReducer } from './curriculum-reducer';
-//import { allCurriculumReducer } from "./all-curriculum-reducer";
 import { getSkillsByCategoryIdReducer } from "./get-skill-category-reducer";
 
 export interface IGetAllSkillState {
@@ -82,26 +78,17 @@ export interface IViewAllVisualizationsState {
 export interface IState {
   //getAllSkills:IGetAllSkillState
   skillsByCategoryId: IGetSkillsByCategoryIdState;
-  //createCurriculum: ICurriculumState;
   allCategory: ICategoriesState;
-  //updateCurriculum: IViewCurriculumState;
-  //getCurriculumById: IGetCurriculumByIdState;
   skills: ISkillState;
-  //curriculum: ICurriculumState;
-  // allCurriculum: IAllCurriculumState;
   curriculum: ICurriculumState;
   allVisualizations: IViewAllVisualizationsState;
 }
 
 export const state = combineReducers<IState>({
-  //updateCurriculum: updateReducer,
-  //getCurriculumById: getCurriculumByIdReducer,
   //getAllSkills:getAllSkillsReducer,
   skillsByCategoryId: getSkillsByCategoryIdReducer,
   skills: skillsReducer,
   curriculum: curriculumReducer,
-  //allCurriculum: allCurriculumReducer,
-  //createCurriculum: allCurriculumReducer,
   allCategory: categoriesReducer,
   allVisualizations: visualizationReducer
 });
