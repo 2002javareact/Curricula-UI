ca0eb8172df4a1b7702f6c51d45ffb2c21db8c0e
tasayed <taqrim.sayed@gmail.com>
2020-04-15 10:01:46 -0400

    Gerard did this

--- curriculum-ui/src/components/view-all-skill-component/ViewAllSkillsComponent.tsx
+++ curriculum-ui/src/components/view-all-skill-component/ViewAllSkillsComponent.tsx
@@ -68,8 +68,20 @@ export class ViewAllSkillsComponent extends React.Component<IViewAllSkillsProps,
 
             return (
                 <>
+
+{/* <div className="col-3 mx-1 my-1 ">
+           <Card style={{ width: '20rem' }} className=" p-1 visualizationCard shadow-custom m-auto">
+                <CardTitle>{skill.skillName}</CardTitle>
+                <CardText style={{color: skill.category.categoryColor}}>{skill.category.categoryName} ███</CardText>
+                <Row className="d-flex justify-content-center">
+                <Button onClick = {this.setRedirect} className="col-4"color="info">Update</Button>
+                <Button onClick = {this.delete(skill.skillId)} className="col-4" color="danger">Delete</Button>                
+                </Row>
+                
+               </Card>
+               </div> */}
                 <div className="col-3 mx-1 my-1 ">
-                <Card style={{ width: '20rem' }} className=" p-1 visualizationCard shadow-custom">
+                <Card className=" p-1 col-8 w-100 isualizationCard shadow-custom">
                 <CardTitle>{skill.skillName}</CardTitle>
                 <CardText style={{color: skill.category.categoryColor}}>{skill.category.categoryName} ███</CardText>
                 {this.renderRedirect()}
